db0b723667c7e0f9777222a8ac309c8fdcc7787b
Temp71 <deezermail420@gmail.com>
2026-02-15 09:11:05 +0100

    Refactor auth method retrieval using index
    Update authentication method handling to use index.

--- api_client.py
+++ api_client.py
@@ -6,9 +6,12 @@ url = addon.getSetting("jellyseerr_url").rstrip("/")
 username = addon.getSetting("jellyseerr_username")
 password = addon.getSetting("jellyseerr_password")
 api_token = addon.getSetting("jellyseerr_api_token")
-auth_method = addon.getSetting("auth_method")
+auth_method_index = addon.getSetting("auth_method")
 
-if not auth_method:
+# type="select" stores the numeric index: 0 = Password, 1 = API Token
+if auth_method_index == "1":
+    auth_method = "api_token"
+else:
     auth_method = "password"
 
 client = JellyseerrClient(url, username, password, api_token, auth_method)
